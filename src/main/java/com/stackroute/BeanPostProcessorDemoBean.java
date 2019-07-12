c8a4efa93da8a4b3b3bbf5afe16173e580ebc0bf
Sudip-byte <sudiprana9729@gmail.com>
2019-07-12 08:48:56 +0530

    bean_post_process

---
+++ src/main/java/com/stackroute/BeanPostProcessorDemoBean.java
@@ -0,0 +1,18 @@
+package com.stackroute;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+
+public class BeanPostProcessorDemoBean implements BeanPostProcessor {
+
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        System.out.println("Before initialization bean name is"+beanName);
+        return bean;
+    }
+
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        System.out.println("after initialization bean name is"+beanName);
+        return bean;
+    }
+
+}
